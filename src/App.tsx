f2b1293889a52c3a62817fcadb05ff354fc2e61c
Karol Musur <kamusur@gmail.com>
2021-03-25 19:56:55 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <Navbar>
         <Navbar.Group align={Alignment.LEFT}>
-          <Navbar.Heading>Blueprint</Navbar.Heading>
+          <Navbar.Heading>Schedules</Navbar.Heading>
           <Navbar.Divider />
           <Button className="bp3-minimal" icon="home" text="Home" />
           <Button className="bp3-minimal" icon="document" text="Files" />
