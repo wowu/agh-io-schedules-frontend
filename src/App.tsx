4731bf090965e8a9114021b066342ac12feb02df
Karol Musur <kamusur@gmail.com>
2021-03-25 19:53:28 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { Navbar, Button, Alignment } from '@blueprintjs/core';
 
 import Home from './components/Home';
 import Page1 from './components/Page1';
@@ -7,6 +8,15 @@ import Page2 from './components/Page2';
 function App() {
   return (
     <Router>
+      <Navbar>
+        <Navbar.Group align={Alignment.LEFT}>
+          <Navbar.Heading>Blueprint</Navbar.Heading>
+          <Navbar.Divider />
+          <Button className="bp3-minimal" icon="home" text="Home" />
+          <Button className="bp3-minimal" icon="document" text="Files" />
+        </Navbar.Group>
+      </Navbar>
+
       <div>
         <nav>
           <ul>
