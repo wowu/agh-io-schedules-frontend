19ff43e3463021378519db17b580045a71502e52
szelemeh <subzarro6@gmail.com>
2021-04-24 19:27:46 +0200

    Move UpdateSchedulesModal to separate component

---
+++ src/components/UpdateScheduleModal.tsx
@@ -0,0 +1,197 @@
+import { Modal, Button, Alert } from 'antd';
+import Dragger from 'antd/es/upload/Dragger';
+import { useState } from 'react';
+import { InboxOutlined } from '@ant-design/icons';
+import { ScheduleService } from '../services/ScheduleService';
+import ImportCollisions from '../components/ImportCollisions';
+import ImportError from '../components/ImportError';
+import ImportNetworkFailure from '../components/ImportNetworkFailure';
+
+
+enum UpdateFileStatus {
+  default,
+  success,
+  collisions,
+  error,
+  networkFailure,
+}
+
+export default function UpdateScheduleModal(props: any) {
+  const { visible } = props;
+
+  const [uploading, setUploading] = useState<boolean>(false);
+  const [updateFile, setUpdateFile] = useState<any>([]);
+  let [updateSuccessData, setUpdateSuccessData] = useState<any>();
+  let [updateCollisionsData, setUpdateCollisionsData] = useState<any>();
+  let [updateStatus, setUpdateStatus] = useState<UpdateFileStatus>(UpdateFileStatus.default);
+  let [updateResponse, setUpdateResponse] = useState<{ status: number; statusText: string }>({
+    status: -1,
+    statusText: '',
+  });
+  const [updateVisible, setUpdateVisible] = useState<boolean>(visible);
+  let [updateError, setUpdateError] = useState<{ message: string }>({ message: '' });
+  console.log("insode", visible);
+  console.log("insode oth", updateVisible);
+
+  const handleUploadedSuccess = () => {
+    setUpdateFile([]);
+    setUpdateStatus(UpdateFileStatus.default);
+    setUpdateVisible(false);
+    // TODO Refresh current schedule
+  };
+
+
+  const onNetworkFailure = (error: { message: string }) => {
+    console.log(error);
+    setUpdateError(error);
+    setUpdateStatus(UpdateFileStatus.networkFailure);
+  };
+
+  const onOtherError = (response: any) => {
+    setUpdateResponse(response);
+    setUpdateStatus(UpdateFileStatus.error);
+  };
+
+  const onUploadCollisions = (collisionsData: any) => {
+    setUpdateCollisionsData({ schedulesWithConflicts: [collisionsData] });
+    setUpdateStatus(UpdateFileStatus.collisions);
+  };
+
+  const onUploadSuccess = (newSchedulesData: any) => {
+    setUpdateSuccessData(newSchedulesData);
+    setUpdateStatus(UpdateFileStatus.success);
+    setUpdateFile([]);
+  };
+
+  const scheduleDataRequiredToUpload = {
+    id: 1,
+  };
+
+  const uploadNewSchedule = async () => {
+    const formData = new FormData();
+    formData.append('files[]', updateFile[0]);
+    setUploading(true);
+    try {
+      const { response, data } = await ScheduleService.updateSchedule(
+        formData,
+        scheduleDataRequiredToUpload.id,
+        false
+      ); //TODO: failure param only for development
+      if (response.ok) {
+        onUploadSuccess(data);
+      } else if (response.status == 400) {
+        onUploadCollisions(data);
+      } else {
+        onOtherError(response);
+      }
+    } catch (error) {
+      onNetworkFailure(error.error);
+    } finally {
+      setUploading(false);
+    }
+  };
+
+  const showModal = () => {
+    setUpdateVisible(true);
+  };
+
+  const showMessage = () => {
+    switch (updateStatus) {
+      case UpdateFileStatus.default:
+        return <></>;
+      case UpdateFileStatus.success:
+        return (
+          <Alert
+            message="Nie znaleziono kolizji w nowej wersji."
+            description="Zaktualizowano harmonogram."
+            type="success"
+            showIcon
+          />
+        );
+      case UpdateFileStatus.collisions:
+        return ImportCollisions(updateCollisionsData);
+      case UpdateFileStatus.error:
+        return ImportError(updateResponse);
+      case UpdateFileStatus.networkFailure:
+        return ImportNetworkFailure(updateError);
+    }
+  };
+
+  const updateButtons = (): any => {
+    switch (updateStatus) {
+      case UpdateFileStatus.success: {
+        return [
+          <Button key="back" onClick={handleUploadedSuccess}>
+            Zamknij
+          </Button>,
+        ];
+      }
+      default: {
+        return [
+          <Button
+            key="back"
+            onClick={() => {
+              setUpdateVisible(false);
+              setUpdateFile([]);
+            }}
+          >
+            Zamknij
+          </Button>,
+          <Button
+            key="submit"
+            type="primary"
+            loading={uploading}
+            onClick={uploadNewSchedule}
+            disabled={updateFile.length === 0}
+          >
+            Wyślij harmonogram
+          </Button>,
+        ];
+      }
+    }
+  };
+
+  let draggerProps = {
+    onRemove: (file: any) => {
+      setUpdateFile([]);
+    },
+    beforeUpload: (file: File) => {
+      setUpdateFile([file]);
+      return false;
+    },
+    multiple: false,
+    maxCount: 1,
+    fileList: updateFile,
+    accept:
+      'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel',
+  };
+
+  return (
+    <>
+      <Button type="primary" onClick={showModal}>
+        Wyślij nową wersję harmonogramu
+        </Button>
+      <Modal
+        visible={visible || updateVisible}
+        title="Zaktualizuj plik harmonogramu"
+        footer={updateButtons()}
+      >
+        <>
+          {updateStatus != UpdateFileStatus.success && (
+            <Dragger {...draggerProps}>
+              <p className="ant-upload-drag-icon">
+                <InboxOutlined />
+              </p>
+              <p className="ant-upload-text">Kliknij lub przeciągnij plik z harmonogramem</p>
+              <p className="ant-upload-hint">
+                Dopuszczalne formaty: <br /> xls, xlsx
+                </p>
+            </Dragger>
+          )}
+          <br />
+          {showMessage()}
+        </>
+      </Modal>
+    </>
+  )
+}
--- src/pages/Schedule.tsx
+++ src/pages/Schedule.tsx
@@ -1,15 +1,11 @@
 import CenteredHeader from '../components/CenteredHeader';
 import React, { useState } from 'react';
-import { Alert, Badge, Button, Calendar, Col, List, Modal, Row } from 'antd';
+import { Badge, Button, Calendar, Col, List, Modal, Row } from 'antd';
 import moment from 'moment';
 import { Link } from 'react-router-dom';
 import ReactJson from 'react-json-view';
 import { ScheduleService } from '../services/ScheduleService';
-import Dragger from 'antd/es/upload/Dragger';
-import { InboxOutlined } from '@ant-design/icons';
-import ImportCollisions from '../components/ImportCollisions';
-import ImportError from '../components/ImportError';
-import ImportNetworkFailure from '../components/ImportNetworkFailure';
+import UpdateScheduleModal from '../components/UpdateScheduleModal';
 
 enum UpdateFileStatus {
   default,
@@ -80,10 +76,6 @@ function monthCellRender(value: moment.Moment) {
   ) : null;
 }
 
-const scheduleDataRequiredToUpload = {
-  id: 1,
-};
-
 const data = [
   {
     title: 'Event 1',
@@ -102,149 +94,9 @@ const data = [
 export default function Schedule() {
   const [schedules, setSchedules] = useState<any>();
 
-  const [uploading, setUploading] = useState<boolean>(false);
-  const [updateFile, setUpdateFile] = useState<any>([]);
-  let [updateSuccessData, setUpdateSuccessData] = useState<any>();
-  let [updateCollisionsData, setUpdateCollisionsData] = useState<any>();
-  let [updateStatus, setUpdateStatus] = useState<UpdateFileStatus>(UpdateFileStatus.default);
-  let [updateResponse, setUpdateResponse] = useState<{ status: number; statusText: string }>({
-    status: -1,
-    statusText: '',
-  });
-  const [updateVisible, setUpdateVisible] = useState<boolean>(false);
-  let [updateError, setUpdateError] = useState<{ message: string }>({ message: '' });
-
   const loadSchedules = async () => {
     const json = await ScheduleService.getSchedule(1);
     setSchedules(json);
-  };
-
-  const showMessage = () => {
-    switch (updateStatus) {
-      case UpdateFileStatus.default:
-        return <></>;
-      case UpdateFileStatus.success:
-        return (
-          <Alert
-            message="Nie znaleziono kolizji w nowej wersji."
-            description="Zaktualizowano harmonogram."
-            type="success"
-            showIcon
-          />
-        );
-      case UpdateFileStatus.collisions:
-        return ImportCollisions(updateCollisionsData);
-      case UpdateFileStatus.error:
-        return ImportError(updateResponse);
-      case UpdateFileStatus.networkFailure:
-        return ImportNetworkFailure(updateError);
-    }
-  };
-
-  const onNetworkFailure = (error: { message: string }) => {
-    console.log(error);
-    setUpdateError(error);
-    setUpdateStatus(UpdateFileStatus.networkFailure);
-  };
-
-  const onOtherError = (response: any) => {
-    setUpdateResponse(response);
-    setUpdateStatus(UpdateFileStatus.error);
-  };
-
-  const onUploadCollisions = (collisionsData: any) => {
-    setUpdateCollisionsData({ schedulesWithConflicts: [collisionsData] });
-    setUpdateStatus(UpdateFileStatus.collisions);
-  };
-
-  const onUploadSuccess = (newSchedulesData: any) => {
-    setUpdateSuccessData(newSchedulesData);
-    setUpdateStatus(UpdateFileStatus.success);
-    setUpdateFile([]);
-  };
-
-  const uploadNewSchedule = async () => {
-    const formData = new FormData();
-    formData.append('files[]', updateFile[0]);
-    setUploading(true);
-    try {
-      const { response, data } = await ScheduleService.updateSchedule(
-        formData,
-        scheduleDataRequiredToUpload.id,
-        false
-      ); //TODO: failure param only for development
-      if (response.ok) {
-        onUploadSuccess(data);
-      } else if (response.status == 400) {
-        onUploadCollisions(data);
-      } else {
-        onOtherError(response);
-      }
-    } catch (error) {
-      onNetworkFailure(error.error);
-    } finally {
-      setUploading(false);
-    }
-  };
-
-  const showModal = () => {
-    setUpdateVisible(true);
-  };
-
-  const handleUploadedSuccess = () => {
-    setUpdateFile([]);
-    setUpdateStatus(UpdateFileStatus.default);
-    setUpdateVisible(false);
-    // TODO Refresh current schedule
-  };
-
-  let draggerProps = {
-    onRemove: (file: any) => {
-      setUpdateFile([]);
-    },
-    beforeUpload: (file: File) => {
-      setUpdateFile([file]);
-      return false;
-    },
-    multiple: false,
-    maxCount: 1,
-    fileList: updateFile,
-    accept:
-      'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel',
-  };
-
-  const updateButtons = (): any => {
-    switch (updateStatus) {
-      case UpdateFileStatus.success: {
-        return [
-          <Button key="back" onClick={handleUploadedSuccess}>
-            Zamknij
-          </Button>,
-        ];
-      }
-      default: {
-        return [
-          <Button
-            key="back"
-            onClick={() => {
-              setUpdateVisible(false);
-              setUpdateFile([]);
-            }}
-          >
-            Zamknij
-          </Button>,
-          <Button
-            key="submit"
-            type="primary"
-            loading={uploading}
-            onClick={uploadNewSchedule}
-            disabled={updateFile.length === 0}
-          >
-            Wyślij harmonogram
-          </Button>,
-        ];
-      }
-    }
   };
 
   return (
@@ -270,30 +122,7 @@ export default function Schedule() {
         </Col>
       </Row>
       <Row>
-        <Button type="primary" onClick={showModal}>
-          Wyślij nową wersję harmonogramu
-        </Button>
-        <Modal
-          visible={updateVisible}
-          title="Zaktualizuj plik harmonogramu"
-          footer={updateButtons()}
-        >
-          <>
-            {updateStatus != UpdateFileStatus.success && (
-              <Dragger {...draggerProps}>
-                <p className="ant-upload-drag-icon">
-                  <InboxOutlined />
-                </p>
-                <p className="ant-upload-text">Kliknij lub przeciągnij plik z harmonogramem</p>
-                <p className="ant-upload-hint">
-                  Dopuszczalne formaty: <br /> xls, xlsx
-                </p>
-              </Dragger>
-            )}
-            <br />
-            {showMessage()}
-          </>
-        </Modal>
+        <UpdateScheduleModal />
       </Row>
     </>
   );
