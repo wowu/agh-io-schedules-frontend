7bb43614e5723c42be0397a66804108a5209afb7
Karol Musur <kamusur@gmail.com>
2021-05-23 22:26:27 +0200

    Custom notifications for user

--- src/services/NotificationService.tsx
+++ src/services/NotificationService.tsx
@@ -5,6 +5,11 @@ export interface Notification {
   unit: 'minute' | 'hour' | 'day';
 }
 
+export type UserNotifications = {
+  notifications?: Notification[];
+  default: boolean;
+};
+
 export class NotificationService {
   static async setGlobalNotifications(notifications: Notification[]) {
     try {
@@ -28,5 +33,32 @@ export class NotificationService {
         unit: 'day',
       },
     ]);
+  }
+
+  static async setUserNotifications(userNotifications: UserNotifications) {
+    try {
+      const body = JSON.stringify(userNotifications);
+      const response = await ApiAdapter.put('/api/me/notifications', body);
+      const data = await response.json();
+      return Promise.resolve({ response, data });
+    } catch (error) {
+      console.error('setUserNotifications: ', error);
+    }
+  }
+
+  static async getUserNotifications(): Promise<UserNotifications> {
+    // const response = await ApiAdapter.get('/api/me/notifications');
+    // const data = await response.json();
+    // return Promise.resolve(data);
+
+    return Promise.resolve({
+      default: false,
+      notifications: [
+        {
+          value: 1,
+          unit: 'day',
+        },
+      ],
+    });
   }
 }
